dafa48d3a7f53134e275eeb18096026ea38c8bad
TusaA <adina.tusa@paddypowerbetfair.com>
2017-05-03 09:15:37 +0300

    FedEx-ActionBar customized

--- app/src/main/java/com/betfair/fedex/MainActivity.java
+++ app/src/main/java/com/betfair/fedex/MainActivity.java
@@ -1,6 +1,9 @@
 package com.betfair.fedex;
 
+import android.app.ActionBar;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
