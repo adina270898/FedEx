ab12b485e0af01b780fa0a22cce5f36c26e1b789
TusaA <adina.tusa@paddypowerbetfair.com>
2017-05-05 11:48:31 +0300

    FedEx-Added Send Money functionality

--- app/src/main/java/com/betfair/fedex/activities/SendMoney.java
+++ app/src/main/java/com/betfair/fedex/activities/SendMoney.java
@@ -1,31 +1,77 @@
 package com.betfair.fedex.activities;
 
 import android.content.Intent;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
+import android.nfc.NfcEvent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.betfair.fedex.R;
 import com.betfair.fedex.utils.IOUtils;
 
-import static android.provider.AlarmClock.EXTRA_MESSAGE;
-
-public class SendMoney extends AppCompatActivity {
+public class SendMoney extends AppCompatActivity implements NfcAdapter.CreateNdefMessageCallback, NfcAdapter.OnNdefPushCompleteCallback {
 
     private static final String FILE_NAME = "fedEx";
+    private NfcAdapter nfcAdapter;
+    private EditText txtAmount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_send_money);
+        txtAmount = (EditText) findViewById(R.id.txtAmount);
 
-        // Get the Intent that started this activity and extract the string
-        Intent intent = getIntent();
-        String message = intent.getExtras().getString(EXTRA_MESSAGE);
-        // Capture the layout's TextView and set the string as its text
-        TextView textView = (TextView) findViewById(R.id.txtAmount);
-        String cardNumber = IOUtils.readFromInternalStorage(this.getApplicationContext(), FILE_NAME);
-        textView.setText(message + " " + cardNumber);
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        if (nfcAdapter == null) {
+            Toast.makeText(SendMoney.this, "NFC is not active. Please activate it!", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(SendMoney.this, "Set Callback(s)", Toast.LENGTH_LONG).show();
+            nfcAdapter.setNdefPushMessageCallback(this, this);
+            nfcAdapter.setOnNdefPushCompleteCallback(this, this);
+        }
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        setIntent(intent);
+    }
+
+    @Override
+    public void onNdefPushComplete(NfcEvent event) {
+        final String eventString = "Amount sent";
+        runOnUiThread(new Runnable() {
+
+            @Override
+            public void run() {
+                Toast.makeText(getApplicationContext(), eventString, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    @Override
+    public NdefMessage createNdefMessage(NfcEvent event) {
+        String securedCardNumber = IOUtils.readFromInternalStorage(this.getApplicationContext(), FILE_NAME);
+        String cardNumber = securedCardNumber.substring(0, securedCardNumber.length() - 1);
+        String stringOut = cardNumber + txtAmount.getText().toString();
+        byte[] bytesOut = stringOut.getBytes();
+
+        NdefRecord ndefRecordOut = new NdefRecord(
+                NdefRecord.TNF_MIME_MEDIA,
+                "text/plain".getBytes(),
+                new byte[]{},
+                bytesOut);
+
+        NdefMessage ndefMessageout = new NdefMessage(ndefRecordOut);
+        return ndefMessageout;
+    }
+
 }
