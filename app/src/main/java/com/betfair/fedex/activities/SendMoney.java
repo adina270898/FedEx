3847d7618a6ade77c2d6906e50d836360fb8fa6e
Oana Groza <oana.groza@betfair.com>
2017-05-05 10:37:25 +0300

    Fedex- Added Back button Functionality, Application Icon, Locked App screen in portrait mode

--- app/src/main/java/com/betfair/fedex/activities/SendMoney.java
+++ app/src/main/java/com/betfair/fedex/activities/SendMoney.java
@@ -1,8 +1,10 @@
 package com.betfair.fedex.activities;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.betfair.fedex.R;
@@ -20,12 +22,27 @@ public class SendMoney extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_send_money);
 
-        // Get the Intent that started this activity and extract the string
+        //Block app screen in portrait mode
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+          // Get the Intent that started this activity and extract the string
         Intent intent = getIntent();
         String message = intent.getExtras().getString(EXTRA_MESSAGE);
         // Capture the layout's TextView and set the string as its text
         TextView textView = (TextView) findViewById(R.id.txtAmount);
         String cardNumber = IOUtils.readFromInternalStorage(this.getApplicationContext(), FILE_NAME);
         textView.setText(message + " " + cardNumber);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case android.R.id.home:
+                // app icon in action bar clicked; goto parent activity.
+                this.finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
